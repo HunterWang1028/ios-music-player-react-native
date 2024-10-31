169789d4b9db0f874d051c94598b2111d92598b8
Hunter <s092284s@gmail.com>
2024-10-31 20:54:31 +0800

    add animated icons on the TrackListItem artwork

--- src/components/TrackListItem.tsx
+++ src/components/TrackListItem.tsx
@@ -1,11 +1,12 @@
 import { unknownTrackImageUri } from '@/constants/images'
 import { colors, fontSize } from '@/constants/tokens'
 import { defaultStyles } from '@/styles'
-import { Entypo } from '@expo/vector-icons'
+import { Entypo, Ionicons } from '@expo/vector-icons'
 import React from 'react'
 import { StyleSheet, Text, TouchableHighlight, View } from 'react-native'
 import FastImage from 'react-native-fast-image'
-import { Track, useActiveTrack } from 'react-native-track-player'
+import LoaderKit from 'react-native-loader-kit'
+import { Track, useActiveTrack, useIsPlaying } from 'react-native-track-player'
 
 export type TrackListItemProps = {
 	track: Track
@@ -13,6 +14,7 @@ export type TrackListItemProps = {
 }
 
 const TrackListItem = ({ track, onTrackSelect: handleTrackSelect }: TrackListItemProps) => {
+	const { playing } = useIsPlaying()
 	const isActiveTrack = useActiveTrack()?.url === track.url
 	return (
 		<TouchableHighlight onPress={() => handleTrackSelect(track)}>
@@ -28,6 +30,21 @@ const TrackListItem = ({ track, onTrackSelect: handleTrackSelect }: TrackListIte
 							opacity: isActiveTrack ? 0.6 : 1,
 						}}
 					/>
+					{isActiveTrack &&
+						(playing ? (
+							<LoaderKit
+								style={styles.trackPlayingIconIndicator}
+								name="LineScaleParty"
+								color={colors.icon}
+							/>
+						) : (
+							<Ionicons
+								style={styles.trackPausedIndicator}
+								name="play"
+								size={24}
+								color={colors.icon}
+							/>
+						))}
 				</View>
 
 				{/* Track title + artist */}
@@ -86,5 +103,17 @@ const styles = StyleSheet.create({
 		columnGap: 14,
 		alignItems: 'center',
 		paddingRight: 20,
+	},
+	trackPlayingIconIndicator: {
+		position: 'absolute',
+		top: 18,
+		left: 16,
+		width: 16,
+		height: 16,
+	},
+	trackPausedIndicator: {
+		position: 'absolute',
+		top: 14,
+		left: 14,
 	},
 })
--- src/components/TracksList.tsx
+++ src/components/TracksList.tsx
@@ -1,6 +1,8 @@
+import { unknownTrackImageUri } from '@/constants/images'
 import { utilsStyles } from '@/styles'
 import React from 'react'
-import { FlatList, FlatListProps, View } from 'react-native'
+import { FlatList, FlatListProps, Text, View } from 'react-native'
+import FastImage from 'react-native-fast-image'
 import TrackPlayer, { Track } from 'react-native-track-player'
 import TrackListItem from './TrackListItem'
 
@@ -29,6 +31,15 @@ const TracksList = ({ tracks, ...flatlistProps }: TracksListProps) => {
 			ItemSeparatorComponent={ItemDivider}
 			contentContainerStyle={{ paddingTop: 10, paddingBottom: 128 }}
 			ListFooterComponent={ItemDivider}
+			ListEmptyComponent={
+				<View>
+					<Text style={utilsStyles.emptyContentText}>No songs found</Text>
+					<FastImage
+						source={{ uri: unknownTrackImageUri, priority: FastImage.priority.normal }}
+						style={utilsStyles.emptyContentImage}
+					/>
+				</View>
+			}
 			renderItem={({ item: track }) => (
 				<TrackListItem track={track} onTrackSelect={handleTrackSelect} />
 			)}
