2cbd61bed80ac994acfb3a24779fb138fab5d981
Hunter <s092284s@gmail.com>
2024-11-01 22:17:22 +0800

    favorite screen

--- src/app/(tabs)/favorites/index.tsx
+++ src/app/(tabs)/favorites/index.tsx
@@ -1,10 +1,28 @@
+import library from '@/assets/data/library.json'
+import TracksList from '@/components/TracksList'
+import { screenPadding } from '@/constants/tokens'
+import { useNavigationSearch } from '@/hooks/useNavigationSearch'
 import { defaultStyles } from '@/styles'
-import { Text, View } from 'react-native'
+import { useMemo } from 'react'
+import { ScrollView, View } from 'react-native'
 
 const FavoriteScreen = () => {
+	const seaech = useNavigationSearch({
+		searchBarOptions: {
+			placeholder: 'Search',
+		},
+	})
+	const favoriteTracks = useMemo(() => {
+		return library.filter((track) => track.rating === 1)
+	}, [])
 	return (
 		<View style={defaultStyles.container}>
-			<Text style={defaultStyles.text}>Favorite screen</Text>
+			<ScrollView
+				style={{ paddingHorizontal: screenPadding.horizontal }}
+				contentInsetAdjustmentBehavior="automatic"
+			>
+				<TracksList tracks={favoriteTracks} scrollEnabled={false} />
+			</ScrollView>
 		</View>
 	)
 }
